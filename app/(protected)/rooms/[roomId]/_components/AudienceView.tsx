ba14479d039254a3a04d1fafd3330c61d7cde5b9
mohammad-zrar <mzrar.dev@gmail.com>
2026-04-18 14:36:36 +0300

    feat: enhance AudienceView component layout; add Leave Stream button functionality

--- app/(protected)/rooms/[roomId]/_components/AudienceView.tsx
+++ app/(protected)/rooms/[roomId]/_components/AudienceView.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { LivestreamPlayer } from "@stream-io/video-react-sdk";
+import { Button } from "@/components/ui/button";
 
 export default function AudienceView({
   callType,
@@ -9,13 +11,22 @@ export default function AudienceView({
   callType: string;
   callId: string;
 }) {
+  const router = useRouter();
+
   return (
-    <div className="h-full">
-      <LivestreamPlayer
-        callType={callType}
-        callId={callId}
-        joinBehavior="live"
-      />
+    <div className="h-full flex flex-col">
+      <div className="flex-1 min-h-0">
+        <LivestreamPlayer
+          callType={callType}
+          callId={callId}
+          joinBehavior="live"
+        />
+      </div>
+      <div className="flex justify-end p-4 border-t bg-background">
+        <Button variant="outline" onClick={() => router.push("/")}>
+          Leave Stream
+        </Button>
+      </div>
     </div>
   );
 }
